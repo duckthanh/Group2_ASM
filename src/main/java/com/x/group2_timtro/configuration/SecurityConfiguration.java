85a58a63f638b5b7cf8d8594129f6e2c5e7f20f5
duckthanh <thanhvttb2004@gmail.com>
2025-10-01 02:34:14 +0700

    authentication1

--- src/main/java/com/x/group2_timtro/configuration/SecurityConfiguration.java
+++ src/main/java/com/x/group2_timtro/configuration/SecurityConfiguration.java
@@ -1,4 +1,40 @@
 package com.x.group2_timtro.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/users", "/auth/login", "/forgot-password").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        SecretKey  secretKeySpec = new SecretKeySpec("secret".getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretKeySpec).build();
+    }
+
+
 }
--- src/main/java/com/x/group2_timtro/service/AuthenticationService.java
+++ src/main/java/com/x/group2_timtro/service/AuthenticationService.java
@@ -2,11 +2,14 @@ package com.x.group2_timtro.service;
 
 import com.x.group2_timtro.dto.request.LoginRequest;
 import com.x.group2_timtro.dto.response.LoginResponse;
+import com.x.group2_timtro.entity.User;
 import com.x.group2_timtro.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,9 +17,20 @@ import org.springframework.stereotype.Service;
 public class AuthenticationService {
 
 
-    public LoginResponse login(LoginRequest request) {
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+    private final UserRepository userRepository;
 
+    public LoginResponse login(LoginRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String passwordHash = user.getPassword();
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        if (!passwordEncoder.matches(password, passwordHash)) {
+            throw new RuntimeException("Invalid password");
+        }
 
 
         // tao token
